f7603715125ce0411638700fce5d94bbeb02a845
Pauline183 <pauline.kipp@gmx.net>
2025-11-20 15:48:53 +0100

    test2

--- Test/test_file_handling.py
+++ Test/test_file_handling.py
@@ -1,5 +1,6 @@
+from Proteosim.file_handling import read_fasta
 def test_read_fasta():
-    tmp_fasta_path = '/Users/paulinekipp/Documents/GitHub/advanced-python_course/data/dummy_proteins.fasta'
+    tmp_fasta_path = 'data/dummy_proteins.fasta'
     protein_map = read_fasta(tmp_fasta_path)
     print(protein_map)
     
@@ -7,5 +8,5 @@ def test_read_fasta():
     # which you expect to be now available as a dictionary
     assert protein_map["A123"] == "DUMMYSEQUENCEONE"
     assert protein_map["B123"] == "DUMMYSEQUENCETWO"
+    
 
-test_read_fasta()
